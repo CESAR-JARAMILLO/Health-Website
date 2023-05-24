9d209b9879bf29854120cc3cdd3b64c728492675
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-24 07:30:02 -0700

    fixed bugs

--- src/app/components/RegisterCard.jsx
+++ src/app/components/RegisterCard.jsx
@@ -1,34 +1,33 @@
 import { Box, Button, FormControl, FormLabel, Heading, Input, Stack, Flex, Link } from '@chakra-ui/react';
-import NextLink from 'next/link';
 
 function RegisterCard() {
   return (
     <Flex align="center" justify="center" minH="100vh">
       <Box maxW="sm" mx="auto" p={6} borderWidth={1} borderRadius="md" boxShadow="md">
         <Heading as="h2" size="lg" mb={6}>Register</Heading>
-        <Stack spacing={4}>
-          <FormControl id="name">
-            <FormLabel>Name</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl id="password">
-            <FormLabel>Password</FormLabel>
-            <Input type="password" />
-          </FormControl>
-          <FormControl id="confirmPassword">
-            <FormLabel>Confirm Password</FormLabel>
-            <Input type="password" />
-          </FormControl>
-          <Button colorScheme="blue">Register</Button>
-        </Stack>
+        <form>
+          <Stack spacing={4}>
+            <FormControl id="name">
+              <FormLabel>Name</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" autoComplete="username" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" autoComplete="new-password" />
+            </FormControl>
+            <FormControl id="confirmPassword">
+              <FormLabel>Confirm Password</FormLabel>
+              <Input type="password" autoComplete="new-password" />
+            </FormControl>
+            <Button colorScheme="blue">Register</Button>
+          </Stack>
+        </form>
         <Box mt={4}>
-          <NextLink href="/login" passHref>
-            <Link color="blue.500">Already registered? Login</Link>
-          </NextLink>
+          <Link color="blue.500" href="/login">Already registered? Login</Link>
         </Box>
       </Box>
     </Flex>
