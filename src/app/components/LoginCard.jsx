b372f21dcd53b4ba97171f192f1d1d8d3f2d425c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-24 07:26:25 -0700

    fixed bugs

--- src/app/components/LoginCard.jsx
+++ src/app/components/LoginCard.jsx
@@ -1,26 +1,25 @@
 import { Box, Button, FormControl, FormLabel, Heading, Input, Stack, Flex, Link } from '@chakra-ui/react';
-import NextLink from 'next/link';
 
 function LoginCard() {
   return (
     <Flex align="center" justify="center" minH="100vh">
       <Box maxW="md" mx="auto" p={6} borderWidth={1} borderRadius="md" boxShadow="md">
         <Heading as="h2" size="lg" mb={6}>Login</Heading>
-        <Stack spacing={4}>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl id="password">
-            <FormLabel>Password</FormLabel>
-            <Input type="password" />
-          </FormControl>
-          <Button colorScheme="blue">Login</Button>
-        </Stack>
+        <form>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" autoComplete="username" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" autoComplete="current-password" />
+            </FormControl>
+            <Button colorScheme="blue">Login</Button>
+          </Stack>
+        </form>
         <Box mt={4}>
-          <NextLink href="/register" passHref>
-            <Link color="blue.500">Not registered? Register</Link>
-          </NextLink>
+          <Link href="/register" color="blue.500">Not registered? Register</Link>
         </Box>
       </Box>
     </Flex>
