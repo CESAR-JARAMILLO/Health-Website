fcd9b83f80b3596f4cc18e661e7c931732e1bafe
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-24 18:39:30 -0700

    created  and added Header component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Providers } from "./providers";
 import { useRouter } from 'next/navigation'
+import Header from './components/Header';
 
 export default function RootLayout({ children }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Providers>
+          <Header />
           <button onClick={signOut}>Sign Out</button>
           {children}
         </Providers>
