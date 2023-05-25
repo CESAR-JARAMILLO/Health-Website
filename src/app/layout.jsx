7c471c14411779e651b23a2381beedcdb26a2f77
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-24 17:08:55 -0700

    configured supabase

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,36 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Providers } from "./providers";
+import { useRouter } from 'next/navigation'
 
 export default function RootLayout({ children }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const router = useRouter()
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      router.refresh()
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [router, supabase])
+
+  const signOut = () => {
+    supabase.auth.signOut()
+  }
+
+
   return (
     <html lang="en">
       <body>
         <Providers>
+          <button onClick={signOut}>Sign Out</button>
           {children}
         </Providers>
       </body>
