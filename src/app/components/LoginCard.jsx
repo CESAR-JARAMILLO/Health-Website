a39521e879d51073ea7c5881a934b2d57b67a53b
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-24 17:07:16 -0700

    configured supabase login

--- src/app/components/LoginCard.jsx
+++ src/app/components/LoginCard.jsx
@@ -1,6 +1,25 @@
+'use client'
+
 import { Box, Button, FormControl, FormLabel, Heading, Input, Stack, Flex, Link } from '@chakra-ui/react';
+import { useState } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation';
 
 function LoginCard() {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const router = useRouter();
+
+  const signIn = () => {
+    supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+    })
+    router.push('/posts')
+  }
+
   return (
     <Flex align="center" justify="center" minH="100vh">
       <Box maxW="md" mx="auto" p={6} borderWidth={1} borderRadius="md" boxShadow="md">
@@ -9,13 +28,13 @@ function LoginCard() {
           <Stack spacing={4}>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
-              <Input type="email" autoComplete="username" />
+              <Input onChange={e => setEmail(e.target.value)} type="email" autoComplete="username" />
             </FormControl>
             <FormControl id="password">
               <FormLabel>Password</FormLabel>
-              <Input type="password" autoComplete="current-password" />
+              <Input onChange={e => setPassword(e.target.value)} type="password" autoComplete="current-password" />
             </FormControl>
-            <Button colorScheme="blue">Login</Button>
+            <Button onClick={signIn} colorScheme="blue">Login</Button>
           </Stack>
         </form>
         <Box mt={4}>
