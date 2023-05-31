d1727f62e307dc624205b37eb02447633ffc1c41
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-31 15:59:46 -0700

    added global user state

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,27 +7,39 @@ import { useRouter } from 'next/navigation'
 import Header from './components/Header';
 
 export default function RootLayout({ children }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
-  const router = useRouter()
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  const handleAuthStateChange = (event, session) => {
+    if (event === 'SIGNED_OUT' || event === 'USER_DELETED') {
+      console.log('User signed out');
+      setUser(null);
+      router.refresh();
+    } else if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
+      console.log('User signed in');
+      setUser(true);
+      router.refresh();
+    }
+  };
 
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange(() => {
-      router.refresh()
-    })
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      handleAuthStateChange(event, session);
+    });
 
     return () => {
-      subscription.unsubscribe()
-    }
-  }, [router, supabase])
-
+      subscription.unsubscribe();
+    };
+  }, [supabase, router]);
 
   return (
     <html lang="en">
       <body>
         <Providers>
-          <Header />
+          <Header user={user} />
           {children}
         </Providers>
       </body>
